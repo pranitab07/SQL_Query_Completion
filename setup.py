4334a8ecf5adc56622f79a26cb7ee1f8dd7175f3
Aditya Sarade <aditya.sarade2003@gmail.com>
2025-04-04 18:26:29 +0530

    New branch

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project aims to autocomplete the SQL code and help developers to be productive',
+    author='AISSMS IOIT'26',
+    license='MIT',
+)
